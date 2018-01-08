32118a1da883ab199775dfdec872797e506bbcb6
Ana <anajuliabravoleite@gmail.com>
2018-01-08 15:20:39 +0000

    Testing gender classifiers using sklearn algorithms

---
+++ dtree/dtree.py
@@ -0,0 +1,28 @@
+from sklearn import tree
+
+clf = tree.DecisionTreeClassifier()
+
+# CHALLENGE - create 3 more classifiers...
+
+
+# [height, weight, shoe_size]
+X = [[181, 80, 44], [177, 70, 43], [160, 60, 38], [154, 54, 37], [166, 65, 40],
+     [190, 90, 47], [175, 64, 39],
+     [177, 70, 40], [159, 55, 37], [171, 75, 42], [181, 85, 43]]
+
+Y = ['male', 'male', 'female', 'female', 'male', 'male', 'female', 'female',
+     'female', 'male', 'male']
+
+
+# CHALLENGE - ...and train them on our data
+clf = clf.fit(X, Y)
+
+
+#test the classifier:
+prediction = clf.predict([[190, 70, 43], [158, 62, 37]])
+
+
+# CHALLENGE compare their results and print the best one!
+
+print('DecisionTreeClassifier for gender according to height, weight and shoe_size')
+print('The classification is: {}'.format(prediction))
